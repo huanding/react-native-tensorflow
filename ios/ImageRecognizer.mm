e54157b528c0c062f05a5a89e4bd13f16d5a3f86
huanding <huanding@gmail.com>
2018-01-28 11:35:52 -0800

    Fixed image orientation

--- ios/ImageRecognizer.mm
+++ ios/ImageRecognizer.mm
@@ -2,6 +2,8 @@
 
 #include "TensorFlowInference.h"
 
+#import <ImageIO/ImageIO.h>
+
 #include <string>
 #include <fstream>
 
@@ -29,9 +31,7 @@
     NSNumber * maxResultsResolved = maxResults != nil ? maxResults : [NSNumber numberWithInt:3];
     NSNumber * thresholdResolved = threshold != nil ? threshold : [NSNumber numberWithFloat:0.1];
 
-    NSData * imageData = loadFile(image);
-
-    tensorflow::Tensor tensor = createImageTensor(imageData, "jpg");
+    tensorflow::Tensor tensor = createImageTensor(image);
     [inference feed:@"image_tensor" tensor:tensor];
 
     NSArray * outputNames = [NSArray arrayWithObjects:@"detection_classes", @"detection_scores", @"detection_boxes", @"num_detections", nil];
@@ -78,18 +78,26 @@
     return [resultsSorted subarrayWithRange:NSMakeRange(0, finalSize)];
 }
 
-tensorflow::Tensor createImageTensor(NSData * data, const char* image_type) {
+tensorflow::Tensor createImageTensor(NSString * imageUri) {
+    LOG(INFO) << "Loading image URL " << [imageUri UTF8String];
+    NSURL *url = [NSURL URLWithString:imageUri];
+    if (!url || !url.scheme || !url.host) {
+        url = [NSURL fileURLWithPath:imageUri];
+        if (!url || !url.scheme) {
+            throw std::invalid_argument("Invalid image uri");
+        }
+    }
+    
     int image_width;
     int image_height;
     int image_channels;
-    std::vector<tensorflow::uint8> image_data = imageAsVector(data, image_type, &image_width, &image_height, &image_channels);
-
+    std::vector<tensorflow::uint8> image_data = imageAsVector(url, &image_width, &image_height, &image_channels);
+    
     const int wanted_channels = 3;
-
     tensorflow::Tensor image_tensor(tensorflow::DT_UINT8, tensorflow::TensorShape({1, image_height, image_width, wanted_channels}));
     auto image_tensor_mapped = image_tensor.tensor<unsigned char, 4>();
     tensorflow::uint8* in = image_data.data();
-
+    
     unsigned char * out = image_tensor_mapped.data();
     for (int y = 0; y < image_height; ++y) {
         tensorflow::uint8* in_row = in + (y * image_width * image_channels);
@@ -102,55 +110,111 @@ tensorflow::Tensor createImageTensor(NSData * data, const char* image_type) {
             }
         }
     }
-
+    
     return image_tensor;
 }
 
-std::vector<tensorflow::uint8> imageAsVector(NSData * data, const char* image_type, int* out_width, int* out_height, int* out_channels) {
-
-    CFDataRef file_data_ref =  (__bridge CFDataRef)data;
-    CGDataProviderRef image_provider = CGDataProviderCreateWithCFData(file_data_ref);
-
-    CGImageRef image;
-    if (strcasecmp(image_type, "png") == 0) {
-        image = CGImageCreateWithPNGDataProvider(image_provider, NULL, true,
-                                                 kCGRenderingIntentDefault);
-    } else {
-        try {
-            image = CGImageCreateWithJPEGDataProvider(image_provider, NULL, true,
-                                                      kCGRenderingIntentDefault);
-        } catch( std::exception& e ) {
-            CFRelease(image_provider);
-            CFRelease(file_data_ref);
-            fprintf(stderr, "Unknown image type\n");
-            *out_width = 0;
-            *out_height = 0;
-            *out_channels = 0;
-            return std::vector<tensorflow::uint8>();
-        }
+std::vector<tensorflow::uint8> imageAsVector(NSURL * imageUrl, int* out_width, int* out_height, int* out_channels) {
+    CGImageSourceRef source = CGImageSourceCreateWithURL((CFURLRef)imageUrl, NULL);
+    if(source==NULL) {
+        throw std::invalid_argument("Failed to create image source from url");
     }
-
-    const int width = (int)CGImageGetWidth(image);
-    const int height = (int)CGImageGetHeight(image);
-    LOG(INFO) << "Image width: " << width << " height: " << height;
+    
+    CGImageRef image = CGImageSourceCreateImageAtIndex(source, 0, NULL);
+    if(image==NULL) {
+        throw std::invalid_argument("Failed to create image ref from source");
+    }
+    
+    int orientation = 1;
+    CFDictionaryRef dict = CGImageSourceCopyPropertiesAtIndex(source, 0, NULL);
+    if(dict)
+    {
+        CFNumberRef imageOrientation;
+        if(CFDictionaryGetValueIfPresent(dict, kCGImagePropertyOrientation, (const void **)&imageOrientation)) {
+            if(imageOrientation)
+                CFNumberGetValue(imageOrientation, kCFNumberIntType, &orientation);
+        }
+        CFRelease(dict);
+    }
+    LOG(INFO) << "Image orientation " << orientation;
+    CFRelease(source);
+    
+    int width = (int)CGImageGetWidth(image);
+    int height = (int)CGImageGetHeight(image);
+    int canvasw, canvash;
+    
+    if(orientation<=4)
+    {
+        canvasw = width;
+        canvash = height;
+    }
+    else
+    {
+        canvasw = height;
+        canvash = width;
+    }
+    
     const int channels = 4;
-    CGColorSpaceRef color_space = CGColorSpaceCreateDeviceRGB();
-    const int bytes_per_row = (width * channels);
-    const int bytes_in_image = (bytes_per_row * height);
+    const int bytes_per_row = (canvasw * channels);
+    const int bytes_in_image = (bytes_per_row * canvash);
     std::vector<tensorflow::uint8> result(bytes_in_image);
     const int bits_per_component = 8;
-    CGContextRef context = CGBitmapContextCreate(result.data(), width, height,
-                                                 bits_per_component, bytes_per_row, color_space,
-                                                 kCGImageAlphaPremultipliedLast | kCGBitmapByteOrder32Big);
+    CGColorSpaceRef color_space = CGColorSpaceCreateDeviceRGB();
+    CGContextRef context = CGBitmapContextCreate(
+        result.data(), canvasw, canvash,
+        bits_per_component, bytes_per_row, color_space,
+        kCGImageAlphaPremultipliedLast | kCGBitmapByteOrder32Big);
     CGColorSpaceRelease(color_space);
+    
+    switch(orientation)
+    {
+        case 2:
+            // 2 = 0th row is at the top, and 0th column is on the right - Flip Horizontal
+            CGContextConcatCTM(context, CGAffineTransformMake(-1.0, 0.0, 0.0, 1.0, width, 0.0));
+            break;
+            
+        case 3:
+            // 3 = 0th row is at the bottom, and 0th column is on the right - Rotate 180 degrees
+            CGContextConcatCTM(context, CGAffineTransformMake(-1.0, 0.0, 0.0, -1.0, width, height));
+            break;
+            
+        case 4:
+            // 4 = 0th row is at the bottom, and 0th column is on the left - Flip Vertical
+            CGContextConcatCTM(context, CGAffineTransformMake(1.0, 0.0, 0, -1.0, 0.0, height));
+            break;
+            
+        case 5:
+            // 5 = 0th row is on the left, and 0th column is the top - Rotate -90 degrees and Flip Vertical
+            CGContextConcatCTM(context, CGAffineTransformMake(0.0, -1.0, -1.0, 0.0, height, width));
+            break;
+            
+        case 6:
+            // 6 = 0th row is on the right, and 0th column is the top - Rotate 90 degrees
+            CGContextConcatCTM(context, CGAffineTransformMake(0.0, -1.0, 1.0, 0.0, 0.0, width));
+            break;
+            
+        case 7:
+            // 7 = 0th row is on the right, and 0th column is the bottom - Rotate 90 degrees and Flip Vertical
+            CGContextConcatCTM(context, CGAffineTransformMake(0.0, 1.0, 1.0, 0.0, 0.0, 0.0));
+            break;
+            
+        case 8:
+            // 8 = 0th row is on the left, and 0th column is the bottom - Rotate -90 degrees
+            CGContextConcatCTM(context, CGAffineTransformMake(0.0, 1.0, -1.0, 0.0, height, 0.0));
+            break;
+            
+        default:
+            break;
+    }
+    
     CGContextDrawImage(context, CGRectMake(0, 0, width, height), image);
     CGContextRelease(context);
-    CFRelease(image_provider);
-    CFRelease(file_data_ref);
-
-    *out_width = width;
-    *out_height = height;
+    CGImageRelease(image);
+    
+    *out_width = canvasw;
+    *out_height = canvash;
     *out_channels = channels;
+
     return result;
 }
 
